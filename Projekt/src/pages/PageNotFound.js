b06aac5d2a6a79b2eec3846b3a8501d0e67868aa
Wiktor <wiktor01@op.pl>
2022-01-06 15:15:40 +0100

    Jednak to not found lepsze

--- Projekt/src/pages/PageNotFound.js
+++ Projekt/src/pages/PageNotFound.js
@@ -1,6 +1,7 @@
 import style from "./PageNotFound.module.css";
 import Circe from "../components/formComponents/Circle";
 import stylesCircle from "../components/formComponents/Circle.module.css"
+import NotFound from "../components/UI/NotFound";
 import { useSelector } from "react-redux";
 
 export default function PageNotFound() {
@@ -24,15 +25,7 @@ export default function PageNotFound() {
         <Circe className={stylesCircle.circe8} />
         <Circe className={stylesCircle.circe9} />
       <div className={style.infoContainer}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="#8465ff">
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
-        </svg>
-        <p>Page not found</p>
+        <NotFound>Page not found.</NotFound>
       </div>
     </div>
   );
