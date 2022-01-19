ed405a673531005f3a864ac4f2eb4536ceef3f83
maseuko <91914985+maseuko@users.noreply.github.com>
2022-01-19 09:35:13 +0100

    socket

--- Projekt/src/pages/Form.js
+++ Projekt/src/pages/Form.js
@@ -10,9 +10,9 @@ import { Authoindenty } from "../storage/redux-index";
 import { Link, useNavigate } from "react-router-dom";
 import { Fragment } from "react";
 
-const Form = () => {
+const Form = (props) => {
   const history = useNavigate();
-  
+
   const [inputsInfo, setinputsInfo] = useState({
     mailL: "",
     passwordL: "",
@@ -198,6 +198,7 @@ const Form = () => {
       console.log(token);
       if (res.status === 200) {
         const dateToken = new Date(token.auth.token.expire);
+        props.socket.emit("user_logged_in", token.auth.UID);
 
         console.log("Dane są poprawne!");
         dispatch(
@@ -215,7 +216,7 @@ const Form = () => {
           document.cookie = `rememberToken=${token.auth.rememberMeToken.token}; expires=${dateTokenRemember}`;
         }
         setLoadingSpiner(false);
-        history(`/`,{replace: true})
+        history(`/`, { replace: true });
       }
     } catch (error) {
       console.log(error);
